3e85c47de2d032df5de489d279677dad3899d44e
dmswags <143095902+dmswags@users.noreply.github.com>
2025-07-01 01:36:45 -0400

    Scrape eBay title, condition, and price info

--- scrape.py
+++ scrape.py
@@ -1,7 +1,10 @@
 from configparser import ConfigParser
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+
 import sys
+import time
 
 # Read variables from configuation file
 config = ConfigParser()
@@ -10,11 +13,47 @@ config.read("config.ini")
 profile_path = r"{}".format(config.get("FIREFOX", "profile_path"))
 print(profile_path)
 
-# Configure Firefox options -- MUST 
+# Configure Firefox options
 options = Options()
 options.add_argument("-profile")
 options.add_argument(profile_path)
 
-
 # Create instance of Firefox webdriver
 driver = webdriver.Firefox(options=options)
+
+# Read URL from system arguemnts
+url = sys.argv[1]
+print(url)
+
+# Navigate to listing page
+driver.get(url)
+
+# SCRAPE LISTING PAGE FOR IMPORTANT PRODUCT DETAILS
+attributes = dict()
+
+# Scroll to "bottom" of the webpage
+time.sleep(2)
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(2)
+
+# Attribute: Title
+title_element = driver.find_element(By.CLASS_NAME, "x-item-title__mainTitle")
+attributes["title"] = title_element.text
+
+# Attribute: Condition
+condition_element = driver.find_element(By.CLASS_NAME, "x-item-condition-text")
+temp = (condition_element.text).splitlines()
+temp = temp[0]
+attributes["condition"] = temp
+
+# Attribute: Price
+element = driver.find_element(By.CLASS_NAME, "x-price-primary")
+
+# Remove non-numeric characters with partial string ("US $")
+attributes["price"] = (element.text)[4:]
+
+print(attributes)
+
+driver.close()
+ 
+# Unable to Scrape Item Description (iFrame) + Item Specifics (Color, Country of Origin)
