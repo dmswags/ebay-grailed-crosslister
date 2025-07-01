014b62d7c707464ea95f0fec4fcda71b1fa7efde
dmswags <143095902+dmswags@users.noreply.github.com>
2025-06-30 23:41:57 -0400

    Fixed configparser

--- scrape.py
+++ scrape.py
@@ -1,7 +1,20 @@
 from configparser import ConfigParser
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import sys
 
 # Read variables from configuation file
 config = ConfigParser()
 config.read("config.ini")
 
-profile_path = config.get("FIREFOX", "profile_path")
+profile_path = r"{}".format(config.get("FIREFOX", "profile_path"))
+print(profile_path)
+
+# Configure Firefox options -- MUST 
+options = Options()
+options.add_argument("-profile")
+options.add_argument(profile_path)
+
+
+# Create instance of Firefox webdriver
+driver = webdriver.Firefox(options=options)
